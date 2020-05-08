c1c146099f669df9767c9bb4f91a928d24783d6b
DavidColatti <davidcolatti@gmail.com>
2020-05-08 19:38:27 -0400

    google oauth set up

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-// get is a route handler
-app.get('/', (req, res) => {
-	res.send({ bye: 'buddy' });
-});
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback'
+		},
+		(accessToken, refreshToken, profile, done) => {
+			console.log('access token', accessToken);
+			console.log('refreshToken', refreshToken);
+			console.log('profile', profile);
+		}
+	)
+);
+
+app.get(
+	'/auth/google',
+	passport.authenticate('google', {
+		scope: [ 'profile', 'email' ]
+	})
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 // dynamic PORT binding
 const PORT = process.env.PORT || 5000;
