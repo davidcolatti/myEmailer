3ef76576192eb6ce04c743536235c3cb75ca6546
DavidColatti <davidcolatti@gmail.com>
2020-06-02 12:21:53 -0400

    reducers and redux

--- services/passport.js
+++ services/passport.js
@@ -25,17 +25,18 @@ passport.use(
 			callbackURL: '/auth/google/callback',
 			proxy: true
 		},
-		(accessToken, refreshToken, profile, done) => {
+		async (accessToken, refreshToken, profile, done) => {
 			// checking if the user logged in exists or not
-			User.findOne({ googleId: profile.id }).then((existingUser) => {
-				if (existingUser) {
-					// we already have a record with profile id
-					done(null, existingUser);
-				} else {
-					// we dont have a record, make a new user
-					new User({ googleId: profile.id }).save().then((user) => done(null, user));
-				}
-			});
+			const existingUser = await User.findOne({ googleId: profile.id });
+
+			if (existingUser) {
+				// we already have a record with profile id
+				return done(null, existingUser);
+			}
+
+			// we dont have a record, make a new user
+			const user = await new User({ googleId: profile.id }).save();
+			done(null, user);
 		}
 	)
 );
